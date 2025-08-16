1b7632590cc20e0fb67a39cf7907dd0fc6236f23
TryingToCodeLikeThem <Aeagan0507@gmail.com>
2025-08-16 09:38:01 -0700

    I do not want people use the original type of game engine the original

---
+++ originalcontroller.ts
@@ -0,0 +1,7 @@
+//% blockNamespace=control._impl
+namespace controller {
+    //% blockNamespace=control._impl
+    export namespace Controller {}
+    //% blockNamespace=control._impl
+    export namespace Button {}
+}
